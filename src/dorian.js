d4f1dad42d82079b35424c171aa7ad54cc16745f
Étienne Vallette d'Osia <evo@zengularity.com>
2014-04-18 14:31:29 +0200

    Rendered no longer depends on jsdom

--- src/dorian.js
+++ src/dorian.js
@@ -3,15 +3,35 @@ var Renderer = require("./renderer");
 // TODO change-me
 var fs = require("fs");
 
+function withWindow(content, f) {
+  if (typeof window === "object" && window) {
+    return f(window);
+  } else {
+    require("jsdom").env(
+      content,  // HTML content
+      [],       // JS libs
+      function (errors, window) {
+        return f(window);
+      }
+    );
+  }
+}
+
 function render(cb) {
   fs.mkdir("generated", function (err) {
     if (!err || err.code == 'EEXIST') {
       var content = fs.readFile("to_generate/index.html", "utf8", function (err, content) {
         if (!err) {
-          Renderer.render(content, function (generated) {
-            fs.writeFile("generated/index.html", generated, "utf8", function (err) {
+          withWindow(content, function (window) {
+            Renderer.render(window, function (err, generated) {
               if (!err) {
-                if (cb) cb();
+                fs.writeFile("generated/index.html", generated, "utf8", function (err) {
+                  if (!err) {
+                    if (cb) cb();
+                  } else {
+                    console.log(err);
+                  }
+                });
               } else {
                 console.log(err);
               }
