1b441802f27e76a650d01e1e2a02e5810f54c48a
Étienne Vallette d'Osia <evo@zengularity.com>
2014-04-24 12:15:28 +0200

    server: add "use strict"

--- src/server.js
+++ src/server.js
@@ -3,40 +3,45 @@ var dorian = require("./dorian");
 var Q = require("q");
 var _ = require("underscore");
 
-function withWindow(content, f) {
-  var deferred = Q.defer();
-  if (typeof window === "object" && window) {
-    deferred.resolve(window);
-  } else {
-    require("jsdom").env(
-      content,  // HTML content
-      [],       // JS libs
-      function (err, window) {
-        if (err) {
-          deferred.reject(err);
-        } else {
-          var scripts = window.document.querySelectorAll("script");
-          _.each(scripts, function (script) {
-            var src = script.getAttribute('src');
-            if (src && src.match(/^(.*\/)?dorian.js$/)) {
-              script.parentNode.removeChild(script);
-            }
-          });
-          deferred.resolve(window);
+(function (undefined) {
+  "use strict";
+
+  function withWindow(content, f) {
+    var deferred = Q.defer();
+    if (typeof window === "object" && window) {
+      deferred.resolve(window);
+    } else {
+      require("jsdom").env(
+        content,  // HTML content
+        [],       // JS libs
+        function (err, window) {
+          if (err) {
+            deferred.reject(err);
+          } else {
+            var scripts = window.document.querySelectorAll("script");
+            _.each(scripts, function (script) {
+              var src = script.getAttribute('src');
+              if (src && src.match(/^(.*\/)?dorian.js$/)) {
+                script.parentNode.removeChild(script);
+              }
+            });
+            deferred.resolve(window);
+          }
         }
-      }
-    );
+      );
+    }
+    return deferred.promise;
   }
-  return deferred.promise;
-}
 
-function render() {
-  return Q
-    .nbind(fs.mkdir, fs)("generated").catch(function (err) { if (!err || err.code != 'EEXIST') { throw err; } })
-    .then(function () { return Q.nbind(fs.readFile, fs)("to_generate/index.html", "utf8"); })
-    .then(function (content) { return withWindow(content); })
-    .then(function (window) { return dorian.render(window); })
-    .then(function (generated) { return Q.nbind(fs.writeFile, fs)("generated/index.html", generated, "utf8"); });
-}
+  function render() {
+    return Q
+      .nbind(fs.mkdir, fs)("generated").catch(function (err) { if (!err || err.code != 'EEXIST') { throw err; } })
+      .then(function () { return Q.nbind(fs.readFile, fs)("to_generate/index.html", "utf8"); })
+      .then(function (content) { return withWindow(content); })
+      .then(function (window) { return dorian.render(window); })
+      .then(function (generated) { return Q.nbind(fs.writeFile, fs)("generated/index.html", generated, "utf8"); });
+  }
 
-render().done(function () { console.log("ok"); });
+  render().done(function () { console.log("ok"); });
+
+}());
